0122b717550bc9ca1a7b30c9498062a3fb4e5423
Rein van der Woerd <rwoerd2033@student.landstede.nl>
2016-06-19 17:15:57 +0200

    Fixed typo

--- EventEmitter.js
+++ EventEmitter.js
@@ -10,15 +10,37 @@ class EventEmitter
 
   on (type, listener)
   {
-    if (this.eventTypes[type])
-      this.eventTypes[type].push(listener)
+    if (this.types[type])
+      this.types[type].push(listener)
     else
-      this.eventTypes[type] = [listener]
+      this.types[type] = [listener]
   }
 
   emit (type, ...args)
   {
-    for (const listener of this.eventTypes[type])
-      listener.call(null, ...args)
+    if (this.types[type])
+      for (const listener of this.types[type])
+        listener.call(null, ...args)
   }
 }
+
+/**
+ * Demo
+ */
+class Greetr extends EventEmitter
+{
+  constructor ()
+  {
+    super()
+    this.greeting = 'hello'
+  }
+
+  greet ()
+  {
+    this.emit('greeting')
+  }
+}
+
+const greetr = new Greetr()
+greetr.on('greeting', _ => console.log('Someone greeted'))
+greetr.greet()
